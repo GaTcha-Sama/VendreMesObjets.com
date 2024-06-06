5b3f4aab28b58b2b8400ffd911f890fa0b7cb7e0
Jeremy Louet <jeremy.louet29@hotmail.fr>
2024-06-06 15:35:43 +0200

    Ajout fonctions singup et login pour les users

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const stuffRoutes = require('./routes/stuff')
-
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 const { ReturnDocument } = require('mongodb');
 
@@ -24,6 +24,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
 
