7ad98961d04675f3cb6b16640686b6a890b38886
Jeremy Louet <jeremy.louet29@hotmail.fr>
2024-06-07 11:02:13 +0200

    Version finale du serveur Node

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 
 const stuffRoutes = require('./routes/stuff');
@@ -25,6 +26,7 @@ app.use((req, res, next) => {
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
 
